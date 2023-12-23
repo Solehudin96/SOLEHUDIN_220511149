6fbe0fa29f6867352427ee7903806a4211d9535d
SOLEHUDIN96 <148682462+Solehudin96@users.noreply.github.com>
2023-12-24 06:02:08 +0700

    Add files via upload
    Menghitung Bangun Datar

---
+++ Pertemuan1/Balok.py
@@ -0,0 +1,29 @@
+print("Progam Menghitung Luas dan Volume Balok")
+
+"""
+Nama  : SOLEHUDIN
+NIM   : 220511149
+Kelas : TI22L
+PBO   : Tugas Pertemuan 1 
+
+Keterangan:
+p = Panjang
+l = Lebar
+t = Tinggi
+"""
+
+# Atur Nilai
+p = 6
+l = 7
+t = 8
+
+# Rumus
+luas = (2 * p * l) + (2  * p * t) + (2* l * t)
+volume = (p * l * t)
+
+# Output
+print("Panjang:", p)
+print("Lebar:", l)
+print("Tinggi:", t)
+print("Luas:", luas)
+print("Volume:", volume)
